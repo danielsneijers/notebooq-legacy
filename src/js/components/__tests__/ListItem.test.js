51534bb94f4d7393547b03de9f3149c05dd00c96
Daniel Sneijers <daniel.sneijers@gmail.com>
2016-11-04 12:14:37 +0100

    fix selector and component tests

--- src/js/components/__tests__/ListItem.test.js
+++ src/js/components/__tests__/ListItem.test.js
@@ -8,10 +8,17 @@ import ListItem from '../ListItem'
 import CSS from '../ListItem/ListItem.css'
 
 describe('components > ListItem', () => {
+  const listItemProps = {
+    handleClick: spy()
+  }
+
+  afterEach(() => {
+    listItemProps.handleClick.reset()
+  })
+
   it('renders a list item with the title of the note', () => {
-    const handleClick = spy()
     const wrapper = shallow(
-      <ListItem note={mockNote} handleClick={handleClick} />
+      <ListItem note={mockNote} {...listItemProps} />
     )
     const title = wrapper.find('strong')
 
@@ -21,10 +28,9 @@ describe('components > ListItem', () => {
   })
 
   it('renders a list item with a placeholder title if the note.title is empty', () => {
-    const handleClick = spy()
     const emptyNote = newEmptyNote(1)
     const wrapper = shallow(
-      <ListItem note={emptyNote} handleClick={handleClick} />
+      <ListItem note={emptyNote} {...listItemProps} />
     )
     const title = wrapper.find('strong')
 
@@ -33,16 +39,38 @@ describe('components > ListItem', () => {
     expect(title.text()).to.equal('Untitled note...')
   })
 
-  it('calls handleClick when clicked', () => {
-    const handleClick = spy()
+  it('renders the copy of the note when copy is short', () => {
     const wrapper = shallow(
-      <ListItem note={mockNote} handleClick={handleClick} />
+      <ListItem note={mockNote} {...listItemProps} />
+    )
+    const copy = wrapper.find('small')
+
+    expect(copy.text()).to.equal(`${mockNote.body}\n`)
+  })
+
+  it('renders the excerpt of the note when copy is too long', () => {
+    const longCopyNote = {
+      ...mockNote,
+      body: 'In viverra tellus sed est. In viverra tellus sed est. In viverra tellus sed est.'
+    }
+    const wrapper = shallow(
+      <ListItem note={longCopyNote} {...listItemProps} />
+    )
+    const copy = wrapper.find('small')
+    const expectedCopy = `${longCopyNote.body.substring(0, 65)}\n...`
+
+    expect(copy.text()).to.equal(expectedCopy)
+  })
+
+  it('calls handleClick when clicked', () => {
+    const wrapper = shallow(
+      <ListItem note={mockNote} {...listItemProps} />
     )
 
-    expect(handleClick.called).to.be.false
+    expect(listItemProps.handleClick.called).to.be.false
 
     wrapper.simulate('click')
 
-    expect(handleClick.calledOnce).to.be.true
+    expect(listItemProps.handleClick.calledOnce).to.be.true
   })
 })
--- src/js/components/__tests__/note.test.js
+++ src/js/components/__tests__/note.test.js
@@ -10,9 +10,23 @@ import NoteBodyHtml from 'components/NoteBodyHtml'
 import CSS from '../Note/Note.css'
 
 describe('components > Note', () => {
+  const noteProps = {
+    saveNote: spy(),
+    selectNote: spy(),
+    deleteNote: spy(),
+    newNote: spy(),
+    toggleMarkdownView: spy()
+  }
+
+  afterEach(() => {
+    const propKeys = Object.keys(noteProps)
+
+    propKeys.map((key) => noteProps[key].reset())
+  })
+
   it('renders a title and body when a note is passed', () => {
-    const onSaveNote = spy()
-    const wrapper = shallow(<Note note={mockNote} saveNote={onSaveNote} />)
+    const wrapper = shallow(<Note note={mockNote} {...noteProps} />)
+    const wrapper2 = shallow(<Note note={mockNote} showMarkdown={false} {...noteProps} />)
 
     expect(wrapper.hasClass(CSS.note)).to.be.true
     expect(wrapper.containsAllMatchingElements([
@@ -20,56 +34,52 @@ describe('components > Note', () => {
       <NoteBody body={mockNote.body} />
     ])).to.be.true
 
-    wrapper.setProps({ showMarkdown: false })
-    expect(wrapper.containsAllMatchingElements([
-      <NoteTitle />,
+    expect(wrapper2.containsAllMatchingElements([
+      <NoteTitle title={mockNote.title} />,
       <NoteBodyHtml />
     ])).to.be.true
   })
 
   it('saves the note when onTitleChange is called', () => {
-    const onSaveNote = spy()
-    const wrapper = shallow(<Note note={mockNote} saveNote={onSaveNote} />)
+    const wrapper = shallow(<Note note={mockNote} {...noteProps} />)
     const event = { currentTarget: { value: 'nice change' } }
     const expectedArgument = {
       ...mockNote,
       title: event.currentTarget.value
     }
 
-    expect(onSaveNote.called).to.be.false
+    expect(noteProps.saveNote.called).to.be.false
 
     wrapper.instance().handleTitleChange(event)
 
-    expect(onSaveNote.calledOnce).to.be.true
-    expect(onSaveNote.calledWith(expectedArgument)).to.be.true
+    expect(noteProps.saveNote.calledOnce).to.be.true
+    expect(noteProps.saveNote.calledWith(expectedArgument)).to.be.true
   })
 
   it('saves the note when onCopyChange is called', () => {
-    const onSaveNote = spy()
-    const wrapper = shallow(<Note note={mockNote} saveNote={onSaveNote} />)
+    const wrapper = shallow(<Note note={mockNote} {...noteProps} />)
     const event = { currentTarget: { value: 'nice change' } }
     const expectedArgument = {
       ...mockNote,
       body: event.currentTarget.value
     }
 
-    expect(onSaveNote.called).to.be.false
+    expect(noteProps.saveNote.called).to.be.false
 
     wrapper.instance().handleCopyChange(event)
 
-    expect(onSaveNote.calledOnce).to.be.true
-    expect(onSaveNote.calledWith(expectedArgument)).to.be.true
+    expect(noteProps.saveNote.calledOnce).to.be.true
+    expect(noteProps.saveNote.calledWith(expectedArgument)).to.be.true
   })
 
   it('saves the note when onCopyChange is called', () => {
-    const onDeleteNote = spy()
-    const wrapper = shallow(<Note note={mockNote} deleteNote={onDeleteNote} />)
+    const wrapper = shallow(<Note note={mockNote} {...noteProps} />)
 
-    expect(onDeleteNote.called).to.be.false
+    expect(noteProps.deleteNote.called).to.be.false
 
     wrapper.instance().handleDeleteNote(mockNote.id)
 
-    expect(onDeleteNote.calledOnce).to.be.true
-    expect(onDeleteNote.calledWith(mockNote.id)).to.be.true
+    expect(noteProps.deleteNote.calledOnce).to.be.true
+    expect(noteProps.deleteNote.calledWith(mockNote.id)).to.be.true
   })
 })
